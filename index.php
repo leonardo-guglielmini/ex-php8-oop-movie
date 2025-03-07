13c81554716d888f6fc98c9fe78a0e0f73abcb92
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2025-03-07 12:18:38 +0100

    Add Genre class and integrate it into Movie class constructor

--- index.php
+++ index.php
@@ -1,15 +1,38 @@
 <?php
+class Genre
+{
+    protected $name;
+    protected $description;
+
+    public function __construct($_name, $_description)
+    {
+        $this->name = $_name;
+        $this->description = $_description;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
+
 class Movie
 {
     protected $title;
     protected $length;
     protected $release_year;
+    protected $genre;
 
-    public function __construct($_title, $_length, $_release_year)
+    public function __construct($_title, $_length, $_release_year, Genre $_genre)
     {
         $this->title = $_title;
         $this->length = $_length;
         $this->release_year = $_release_year;
+        $this->genre = $_genre;
     }
 
     public function displayInfo()
@@ -17,11 +40,12 @@ class Movie
         echo "Title: " . $this->title . "<br>";
         echo "Length: " . $this->length . " minutes<br>";
         echo "Release Date: " . $this->release_year . "<br>";
+        echo "Genre: " . $this->genre->getName() . "<br>";
     }
 }
 
-$movie1 = new Movie("The Matrix", 136, "1999");
-$movie2 = new Movie("The Hobbit", 169, "2012")
+$movie1 = new Movie("The Matrix", 136, "1999", new Genre("Sci-fi", "Science fiction movies are films that tell stories about the future, outer space, robots, or aliens, often using special effects to depict these elements."));
+$movie2 = new Movie("The Hobbit", 169, "2012", new Genre("Fantasy", "The fantasy movie genre features magical worlds, mythical creatures, and epic adventures. It often explores good versus evil, destiny, and heroism in imaginative settings."))
 ?>
 
 <!DOCTYPE html>
